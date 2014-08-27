5b34c48ab6d833ea8e1778eb9a78409bbc335f5e
Sterling Radcliff <radcliff@riseup.net>
2014-08-26 17:58:11 -0700

    adds helper method to models for dev happiness

--- quizzes/models.py
+++ quizzes/models.py
@@ -4,6 +4,12 @@ class Question(models.Model):
     question_text = models.CharField(max_length = 200)
     pub_date = models.DateTimeField('date published')
 
+    def __str__(self):
+        return self.question_text
+
 class Answer(models.Model):
     question = models.ForeignKey(Question)
     answer_text = models.CharField(max_length = 200)
+
+    def __str__(self):
+        return self.answer_text
