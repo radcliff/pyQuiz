c1f74dae32939e4cfd85f7b559f411bacaf3183b
Sterling Radcliff <radcliff@riseup.net>
2014-08-28 13:57:31 -0700

    adds route for admin panel

--- pyQuiz/urls.py
+++ pyQuiz/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
@@ -8,3 +8,6 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 )
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls))
+]
