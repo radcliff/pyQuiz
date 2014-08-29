5d989539cceb83c4c279e7d77c99725deebe78a6
Sterling Radcliff <radcliff@riseup.net>
2014-08-28 17:44:42 -0700

    adds views + template for quizzes index

--- quizzes/views.py
+++ quizzes/views.py
@@ -1,13 +1,7 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
-from polls.models import Question, Answer
+from django.shortcuts import render
+from quizzes.models import Question, Answer
 
 def index(request):
-    return HttpResponse("<h1>Hello, world.</h1>")
-
-def show(request, question_id):
-    return HttpResponse('<h1>Hello, world.</h1>' + '<script>console.log(%(question_id)s)</script>' % locals())
-
-def results(request, question_id):
-    response = "This is the results of question %s."
-    return HttpResponse(response % question_id)
+    questions = Question.objects.all()
+    context = { 'questions': questions }
+    return render(request, 'quizzes/index.html', context)
